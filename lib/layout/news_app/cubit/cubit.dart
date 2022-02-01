265328911626de82929971a592595efb18259917
saci zakaria <sacideveloppeur@gmail.com>
2022-02-01 11:35:49 +0100

    NewsInitialState()

--- lib/layout/news_app/cubit/cubit.dart
+++ lib/layout/news_app/cubit/cubit.dart
@@ -4,5 +4,5 @@ import 'package:app_theme_mansour/layout/news_app/cubit/states.dart';
 import 'package:bloc/bloc.dart';
 
 class NewsCubit extends Cubit<NewsStates>{
-  NewsCubit(NewsStates initialState) : super(initialState);
+  NewsCubit() : super(NewsInitialState());
 }
