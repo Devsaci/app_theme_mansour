924531a627a38e7db61350c034c1f03161fda29c
saci zakaria <sacideveloppeur@gmail.com>
2022-02-01 17:05:19 +0100

    NewsCubit / BlocProvider

--- lib/layout/news_app/cubit/cubit.dart
+++ lib/layout/news_app/cubit/cubit.dart
@@ -2,7 +2,12 @@
 
 import 'package:app_theme_mansour/layout/news_app/cubit/states.dart';
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewsCubit extends Cubit<NewsStates>{
   NewsCubit() : super(NewsInitialState());
+  static NewsCubit get(context) => BlocProvider.of(context);
+
+
+
 }
