1a5c47fae8f03a0977968c946d445e7c64e434eb
saci zakaria <sacideveloppeur@gmail.com>
2022-02-01 11:34:44 +0100

    super(initialState) Constructor

--- lib/layout/news_app/cubit/cubit.dart
+++ lib/layout/news_app/cubit/cubit.dart
@@ -1,5 +1,8 @@
 
 
 import 'package:app_theme_mansour/layout/news_app/cubit/states.dart';
+import 'package:bloc/bloc.dart';
 
-class NewsCubit extends Cubit<NewsStates>{}
+class NewsCubit extends Cubit<NewsStates>{
+  NewsCubit(NewsStates initialState) : super(initialState);
+}
