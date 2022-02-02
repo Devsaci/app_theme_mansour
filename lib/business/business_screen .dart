b008882b3329e36a330dbc3db7e7b47e1880d914
saci zakaria <sacideveloppeur@gmail.com>
2022-02-02 10:59:05 +0100

    BusinessScreen

---
+++ lib/business/business_screen .dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class BusinessScreen extends StatelessWidget {
+  const BusinessScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orangeAccent,
+      child: Text("BusinessScreen "),
+    );
+  }
+}
