c6715781e5e419a7b9f5bcf32faae65e1eb9c727
saci zakaria <sacideveloppeur@gmail.com>
2022-02-02 11:54:21 +0100

    AppBarTheme

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,9 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       debugShowCheckedModeBanner: false, ,
-      theme: ThemeData(),
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(),
+    ),
       home: NewsLayout(),
     );
   }
