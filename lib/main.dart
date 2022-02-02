e9ac7399387bf6657b5eb73513340ba224abbc13
saci zakaria <sacideveloppeur@gmail.com>
2022-02-02 11:53:04 +0100

    Format Code index

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,26 @@ void main() {
   runApp(const MyApp());
 }
 
+
+
+
+
+
+
+
+
+
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
 
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'App Theme Manasour ',
+      debugShowCheckedModeBanner: false, ,
       theme: ThemeData(),
       home: NewsLayout(),
     );
