dc260e542a49ac866c322f51904e87667beea3b7
saci zakaria <sacideveloppeur@gmail.com>
2022-02-02 12:01:04 +0100

    scaffoldBackgroundColor

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,19 @@ void main() {
   runApp(const MyApp());
 }
 
-
-
-
-
-
-
-
-
-
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false, ,
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        appBarTheme: AppBarTheme(),
-    ),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+        ),
+      ),
       home: NewsLayout(),
     );
   }
