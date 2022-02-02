a66823ede131534e19efa813b13d53e78395b753
saci zakaria <sacideveloppeur@gmail.com>
2022-02-02 16:27:21 +0100

    cubit

--- lib/layout/news_app/news_layout.dart
+++ lib/layout/news_app/news_layout.dart
@@ -1,43 +1,55 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:app_theme_mansour/layout/news_app/cubit/cubit.dart';
+import 'package:app_theme_mansour/layout/news_app/cubit/states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NewsLayout extends StatelessWidget {
   const NewsLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'News App',
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.business,
-              size: 40,
+    return BlocProvider(
+      create: (BuildContext context) => NewsCubit(),
+      child: BlocConsumer<NewsCubit, NewsStates>(
+        builder: (context, state){
+          var cubit = NewsCubit.get(context);
+          return Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: Text(
+                'News App',
+              ),
             ),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.sports_volleyball,
-              size: 40,
+            bottomNavigationBar: BottomNavigationBar(
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.business,
+                    size: 40,
+                  ),
+                  label: 'Business',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.sports_volleyball,
+                    size: 40,
+                  ),
+                  label: 'Sports',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.science,
+                    size: 40,
+                  ),
+                  label: 'Science',
+                ),
+              ],
             ),
-            label: 'Sports',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.science,
-              size: 40,
-            ),
-            label: 'Science',
-          ),
-        ],
+          );
+        },
+        listener: (context, state){},
       ),
     );
   }
