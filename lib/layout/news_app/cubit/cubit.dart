c69530c1a08e691416aeb2f2580b4434d9a9ce27
saci zakaria <sacideveloppeur@gmail.com>
2022-02-02 17:31:13 +0100

    List<Widget> screens

--- lib/layout/news_app/cubit/cubit.dart
+++ lib/layout/news_app/cubit/cubit.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:app_theme_mansour/layout/news_app/cubit/states.dart';
+import 'package:app_theme_mansour/models/business/business_screen%20.dart';
+import 'package:app_theme_mansour/models/science/science_screen%20.dart';
+import 'package:app_theme_mansour/models/sports/sports_screen%20.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,7 +12,9 @@ class NewsCubit extends Cubit<NewsStates> {
   NewsCubit() : super(NewsInitialState());
 
   static NewsCubit get(context) => BlocProvider.of(context);
+
   int currentIndex = 0;
+
   List<BottomNavigationBarItem> bottomItems = [
     BottomNavigationBarItem(
       icon: Icon(
@@ -29,6 +34,12 @@ class NewsCubit extends Cubit<NewsStates> {
       ),
       label: 'Science',
     ),
+  ];
+
+  List<Widget> screens = [
+    BusinessScreen(),
+    SportsScreen(),
+    ScienceScreen(),
   ];
 
   void changeBottomNavBar(int index) {
