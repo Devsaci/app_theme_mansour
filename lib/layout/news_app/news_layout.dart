6d5f0d78a610c38a1a5af2fe69b096861fb5a3c1
saci zakaria <sacideveloppeur@gmail.com>
2022-02-03 17:30:02 +0100

    query

--- lib/layout/news_app/news_layout.dart
+++ lib/layout/news_app/news_layout.dart
@@ -36,8 +36,9 @@ class NewsLayout extends StatelessWidget {
                 DioHelper.getData(
                   url: 'v2/top-headlines',
                   query: {
-                    '':'',
-                    '':'',
+                    'country':'eg',
+                    'category':'business',
+                    'apiKey':'65f7f556ec76449fa7dc7c0069f040ca',
                   },
                 );
               },
