72c524fccafd29c1a92fc4dd2a8611c10fc4bf49
saci zakaria <sacideveloppeur@gmail.com>
2022-02-03 16:49:57 +0100

    url

--- lib/shared/network/remote/dio_helper.dart
+++ lib/shared/network/remote/dio_helper.dart
@@ -1,22 +1,21 @@
-
-
 import 'package:dio/dio.dart';
 
 class DioHelper {
   static late Dio dio;
 
-  static init(){
+  static init() {
     dio = Dio(
       BaseOptions(
         baseUrl: 'https://newsapi.org/',
         receiveDataWhenStatusError: true,
-
       ),
     );
   }
 
-  void getData(){
-    dio.get("path");
+  void getData({
+    required String url,
+  })
+  {
+    dio.get(url);
   }
-
 }
