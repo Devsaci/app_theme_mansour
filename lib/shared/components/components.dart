20af284c498f98009c295f2b54ef6759af5d1731
saci zakaria <sacideveloppeur@gmail.com>
2022-02-04 16:33:14 +0100

    buildArticleItem / Padding

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,7 +1,55 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 Widget? buildArticleItem() {
-  return null;
+  return Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Row(
+      children: [
+        Container(
+          width: 120.0,
+          height: 120.0,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2015/04/08/15/09/daisies-712892__340.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(width: 20.0),
+        Expanded(
+          child: Container(
+            height: 120.0,
+            child: Column(
+              // mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
+                    'Title',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    maxLines: 4,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Text(
+                  '021212135453',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
