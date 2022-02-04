ad2beed856914d1d8c4b6f940887ecf258a004b8
saci zakaria <sacideveloppeur@gmail.com>
2022-02-04 16:26:36 +0100

    Padding

--- lib/models/business/business_screen .dart
+++ lib/models/business/business_screen .dart
@@ -9,53 +9,54 @@ class BusinessScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 120.0,
-          height: 120.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://cdn.pixabay.com/photo/2015/04/08/15/09/daisies-712892__340.jpg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        SizedBox(width: 20.0),
-        Expanded(
-          child: Container(
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Container(
+            width: 120.0,
             height: 120.0,
-            child: Column(
-              // mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Text(
-                    'Title  Title Title Title Title Title Title Title Title '
-                        'Title Title Title Title Title Title Title Title Title Title'
-                        ' Title Title Title Title Title Title Title Title Title Title ',
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    maxLines: 4,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Text(
-                  '021212135453',
-                  style: TextStyle(
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://cdn.pixabay.com/photo/2015/04/08/15/09/daisies-712892__340.jpg'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-      ],
+          SizedBox(width: 20.0),
+          Expanded(
+            child: Container(
+              height: 120.0,
+              child: Column(
+                // mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Title',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                    '021212135453',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
