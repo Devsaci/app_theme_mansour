df3cf90e65176469ab012e9ba4029a0e006967c5
saci zakaria <sacideveloppeur@gmail.com>
2022-02-04 16:51:02 +0100

    conditional_builder_null_safety: ^0.0.6 / ConditionalBuilder

--- lib/models/business/business_screen .dart
+++ lib/models/business/business_screen .dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:app_theme_mansour/shared/components/components.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -10,6 +11,10 @@ class BusinessScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return buildArticleItem();
+    return ConditionalBuilder(
+      condition: null, // Error
+      fallback: (BuildContext context) {}, // Error
+      builder: (BuildContext context) {}, // Error
+    );
   }
 }
