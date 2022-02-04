844c30bdfbd33db76516957d1e2a61321fece69f
saci zakaria <sacideveloppeur@gmail.com>
2022-02-04 11:09:31 +0100

    image

--- lib/models/business/business_screen .dart
+++ lib/models/business/business_screen .dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class BusinessScreen extends StatelessWidget {
@@ -10,13 +11,22 @@ class BusinessScreen extends StatelessWidget {
     return Row(
       children: [
         Container(
+          width: 350.0,
+          height: 700.0,
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10.0),
-              image: DecorationImage(
-                image: NetworkImage(
-                  'url',
-                ),
-              )),
+            borderRadius: BorderRadius.circular(10.0),
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2015/04/08/15/09/daisies-712892__340.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(width: 20.0),
+        Column(
+          children: [
+
+          ],
         ),
       ],
     );
