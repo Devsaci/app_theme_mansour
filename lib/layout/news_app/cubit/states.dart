43b2223296f49afd45c1984b68fd304de61c42c8
saci zakaria <sacideveloppeur@gmail.com>
2022-02-04 10:11:35 +0100

    NewsLoadingState

--- lib/layout/news_app/cubit/states.dart
+++ lib/layout/news_app/cubit/states.dart
@@ -4,6 +4,8 @@ class NewsInitialState extends NewsStates {}
 
 class NewsBottomNavState extends NewsStates {}
 
+class NewsLoadingState extends NewsStates {}
+
 class NewsGetBusinessLoadingState extends NewsStates {}
 
 class NewsGetBusinessSuccessState extends NewsStates {}
