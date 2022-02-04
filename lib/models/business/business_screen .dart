72045983e517a7e7ca1cf14851c0ce31d5c05696
saci zakaria <sacideveloppeur@gmail.com>
2022-02-04 10:43:43 +0100

    image

--- lib/models/business/business_screen .dart
+++ lib/models/business/business_screen .dart
@@ -11,8 +11,12 @@ class BusinessScreen extends StatelessWidget {
       children: [
         Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-          ),
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage(
+                  'url',
+                ),
+              )),
         ),
       ],
     );
