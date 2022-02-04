a09a738f1ddcd1f6a03053fd1d355634edf00172
saci zakaria <sacideveloppeur@gmail.com>
2022-02-04 10:06:17 +0100

    NewsGetBusinessSuccessState

--- lib/layout/news_app/cubit/states.dart
+++ lib/layout/news_app/cubit/states.dart
@@ -3,3 +3,5 @@ abstract class NewsStates {}
 class NewsInitialState extends NewsStates {}
 
 class NewsBottomNavState extends NewsStates {}
+
+class NewsGetBusinessSuccessState extends NewsStates {}
