7ed85440a39f318fed1e5d362934a0d9dfbd32c8
saci zakaria <sacideveloppeur@gmail.com>
2022-02-12 20:20:33 +0100

    format code

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -3,74 +3,71 @@
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 
-Widget buildArticleItem(article,context) => Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: Row(
-      children: [
-        Container(
-          width: 120.0,
-          height: 120.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            image: DecorationImage(
-              image: NetworkImage('${article['urlToImage']}'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        SizedBox(width: 20.0),
-        Expanded(
-          child: Container(
+Widget buildArticleItem(article, context) => Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Container(
+            width: 120.0,
             height: 120.0,
-            child: Column(
-              // mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Text(
-                    '${article['title']}',
-                    style: Theme.of(context).textTheme.bodyText1,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Text(
-                  '${article['publishedAt']}',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage('${article['urlToImage']}'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
+          SizedBox(width: 20.0),
+          Expanded(
+            child: Container(
+              height: 120.0,
+              child: Column(
+                // mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '${article['title']}',
+                      style: Theme.of(context).textTheme.bodyText1,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                    '${article['publishedAt']}',
+                    style: TextStyle(
+                      fontSize: 10,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
 
 Widget myDivider() => Padding(
-  padding: const EdgeInsetsDirectional.only(
-    start: 20.0,
-    end: 20.0
-  ),
-  child: Container(
-    width: 200,
-    height: 1.0,
-    color: Colors.grey[300],
-  ),
-);
+      padding: const EdgeInsetsDirectional.only(start: 20.0,),
+      child: Container(
+        width: double.infinity,
+        height: 1.0,
+        color: Colors.grey[300],
+      ),
+    );
 
 Widget articleBuilder(list, context) => ConditionalBuilder(
-  condition: list.length > 0,
-  builder: (BuildContext context) => ListView.separated(
-    physics: BouncingScrollPhysics(),
-    itemBuilder: (context, index) => buildArticleItem(list[index], context),
-    separatorBuilder: (context, index) => myDivider(),
-    itemCount: 10,
-  ),
-  fallback: (BuildContext context) => Center(
-    child: CircularProgressIndicator(),
-  ),
-);
+      condition: list.length > 0,
+      builder: (BuildContext context) => ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (context, index) => buildArticleItem(list[index], context),
+        separatorBuilder: (context, index) => myDivider(),
+        itemCount: 10,
+      ),
+      fallback: (BuildContext context) => Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
