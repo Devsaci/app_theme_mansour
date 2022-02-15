e707bb2d50797f5753a018663edc519b4516609d
saci zakaria <sacideveloppeur@gmail.com>
2022-02-15 10:27:28 +0100

    putBoolean

--- lib/shared/network/local/cache_helper.dart
+++ lib/shared/network/local/cache_helper.dart
@@ -3,11 +3,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 class CacheHelper {
   static SharedPreferences? sharedPreferences;
 
-  static init() async{
+  static init() async {
     sharedPreferences = await SharedPreferences.getInstance();
   }
 
-  void putData(){
-
+  void putBoolean({
+    required String key,
+    required bool value,
+  }) {
+    sharedPreferences?.setBool('isDark', value);
   }
 }
