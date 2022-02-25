c510b453cf3f0cb24096dc3ff5a47d4fafd28fb5
saci zakaria <sacideveloppeur@gmail.com>
2022-02-25 15:37:43 +0100

    WebView

--- lib/modules/web_view/web_view_screen.dart
+++ lib/modules/web_view/web_view_screen.dart
@@ -1,15 +1,25 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatelessWidget {
-  const WebViewScreen({Key? key}) : super(key: key);
+   late  String url;
+
+   WebViewScreen(this.url, {Key? key}) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('WebView Screen'),
+        // ignore: prefer_const_constructors
+        title:  Text('WebView Screen'),
+      ),
+      // ignore: prefer_const_constructors
+      body:  WebView(
+        initialUrl: url,
       ),
     );
   }
