58da18fda5a088a2f6de9125e7d5b6996d2d39f6
saci zakaria <sacideveloppeur@gmail.com>
2022-02-26 09:34:08 +0100

    98. Handling Search Issue

--- lib/modules/web_view/web_view_screen.dart
+++ lib/modules/web_view/web_view_screen.dart
@@ -1,23 +1,20 @@
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatelessWidget {
-   late  String url;
+  late String url;
 
-   WebViewScreen(this.url, {Key? key}) : super(key: key);
-
-
+  WebViewScreen(this.url, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         // ignore: prefer_const_constructors
-        title:  Text('WebView Screen'),
+        title: Text('WebView Screen'),
       ),
       // ignore: prefer_const_constructors
-      body:  WebView(
+      body: WebView(
         initialUrl: url,
       ),
     );
